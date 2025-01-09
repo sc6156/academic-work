f7f837ca39ac373f4ca1f97aa971e201421e0687
Scott Cumming <scottcumming156@googlemail.com>
2025-01-09 21:28:08 +0000

    Update apiKey.php

--- MSc/Modules/Secure Web Design - Coorie/Code/assets/scripts/apiKey.php
+++ MSc/Modules/Secure Web Design - Coorie/Code/assets/scripts/apiKey.php
@@ -4,7 +4,7 @@ used to display the Google Map on each accommodation page. It is stored in a
 seperate file for security. */
 
 function getMapsAPIKey() {
-  define('APIKEY', 'AIzaSyC4gi03ixNxx-qlTJcAjLv8ROVJW6Syehk');
+  define('APIKEY', '');
   return APIKEY;
 }
 ?>  
